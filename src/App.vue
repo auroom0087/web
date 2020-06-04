1c91f2a8277997de6bc9dc1c4bf7fd2c47e227bc
Sabynin Anton <auroom0087@yandex.ru>
2020-06-04 17:36:42 +0300

    fc

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+   
+    <v-content>
+      <HelloWorld/>
+      <loginApp />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+import loginApp from './components/loginApp';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+    loginApp
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/loginApp.vue
@@ -0,0 +1,140 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-btn color="orange" dark v-on="on" @click="login = true">Войти</v-btn>
+      </template>
+      <v-card v-if="register">
+        <v-card-title>
+          <span class="headline">Регистрация</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="6">
+                <v-icon ><i class="fas fa-user"></i></v-icon>
+                <v-text-field label="Имя*" prepend-inner-icon='mdi-account' required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="6">
+                <v-text-field
+                  label="Фамилия*"
+                  prepend-inner-icon='mdi-account-outline'
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field label="Email*"  prepend-inner-icon="mdi-email" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field label="Пароль*" prepend-inner-icon='mdi-lock' type="password" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field label="Повторить пароль*" prepend-inner-icon='mdi-lock-open' type="check_password" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field label="Номер телефона" prepend-inner-icon='mdi-phone'></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" >
+                <v-menu
+                    ref="menu"
+                    v-model="menu"
+                    :close-on-content-click="false"
+                    transition="scale-transition"
+                    offset-y
+                    min-width="290px"
+                >
+                    <template v-slot:activator="{ on }">
+                    <v-text-field
+                        v-model="date"
+                        label="Дата рождения"
+                        prepend-icon="mdi-calendar"
+                        readonly
+                        v-on="on"
+                    ></v-text-field>
+                    </template>
+                    <v-date-picker
+                    ref="picker"
+                    v-model="date"
+                    :max="new Date().toISOString().substr(0, 10)"
+                    min="1940-01-01"
+                    @change="save"
+                    ></v-date-picker>
+                </v-menu>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-autocomplete prepend-inner-icon='mdi-city'
+                  :items="['Горно-Алтайск', 'Чемал', 'Майма', 'Кош-Агач', 'Турочак', 'Онугдай', 'Шебалино', 'Кызыл-Осек', 'Усть-Кокса']"
+                  label="Город"
+                  single-line
+                ></v-autocomplete>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*Обязательные поля</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="login = true">Войти</v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false">Закрыть</v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false">Зарегистрироваться</v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card v-if="login">
+        <v-card-title>
+            <span class="headline">Авторизация</span>
+        </v-card-title>
+        <v-card-text>
+            <v-col cols="12">
+                <v-text-field label="Email или Номер телефона" prepend-inner-icon='mdi-email' required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field label="Пароль" prepend-inner-icon='mdi-lock' type="password" required></v-text-field>
+              </v-col>
+        </v-card-text>
+        <v-card-actions>
+            
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="register = true; login = false">Зарегистрироваться</v-btn>
+            <v-btn color="blue darken-1" text @click="dialog = false">Закрыть</v-btn>
+            <v-btn color="blue darken-1" text @click="dialog = false">Войти</v-btn>
+        </v-card-actions>
+        </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+
+<script>
+  export default {
+    data: () => ({
+      dialog: false,
+      login: true,
+      register: false
+    }),
+  }
+</script>
+
+<script>
+    export default {
+    data () {
+        return {
+            dialog: false,
+            isTrue: true
+        }
+    },
+    data: () => ({
+      date: null,
+      menu: false,
+    }),
+    watch: {
+      menu (val) {
+        val && setTimeout(() => (this.$refs.picker.activePicker = 'YEAR'))
+      },
+    },
+    methods: {
+      save (date) {
+        this.$refs.menu.save(date)
+      },
+    },
+  }
+</script>
